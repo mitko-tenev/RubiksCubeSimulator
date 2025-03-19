79d6c6db7cbe908786e7fdebcdf2674f9388bb23
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-03-19 16:37:22 +0200

    Update project structure

---
+++ RubiksCubeSimulator.Tests/Models/RubiksCubeTest.cs
@@ -0,0 +1,66 @@
+﻿using RubiksCubeSimulator.Models;
+
+namespace RubiksCubeSimulator.Tests.Models
+{
+    public class RubiksCubeTest
+    {
+        private readonly RubiksCube _sut;
+
+        public RubiksCubeTest()
+        {
+            _sut = new RubiksCube();
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void GetFace(CubeFace face, char[, ] expected)
+        {
+            var upFace = this._sut.GetFace(face);
+
+            Assert.Equal(expected, upFace);
+        }
+
+        public static IEnumerable<object[]> Data =>
+          new List<object[]>
+          {
+                new object[] { CubeFace.Up, new char[3, 3]
+                {
+                    { 'W', 'W', 'W' },
+                    { 'W', 'W', 'W' },
+                    { 'W', 'W', 'W' }
+                } },
+                new object[] { CubeFace.Down, new char[3, 3]
+                {
+                    { 'Y', 'Y', 'Y' },
+                    { 'Y', 'Y', 'Y' },
+                    { 'Y', 'Y', 'Y' }
+                } },
+                new object[] { CubeFace.Front, new char[3, 3]
+                {
+                    { 'G', 'G', 'G' },
+                    { 'G', 'G', 'G' },
+                    { 'G', 'G', 'G' }
+                } },
+                new object[] { CubeFace.Back, new char[3, 3]
+                {
+                    { 'B', 'B', 'B' },
+                    { 'B', 'B', 'B' },
+                    { 'B', 'B', 'B' }
+                } },
+                new object[] { CubeFace.Left, new char[3, 3]
+                {
+                    { 'O', 'O', 'O' },
+                    { 'O', 'O', 'O' },
+                    { 'O', 'O', 'O' }
+                } }
+                ,
+                new object[] { CubeFace.Right, new char[3, 3]
+                {
+                    { 'R', 'R', 'R' },
+                    { 'R', 'R', 'R' },
+                    { 'R', 'R', 'R' }
+                } }
+          };
+
+    }
+}
