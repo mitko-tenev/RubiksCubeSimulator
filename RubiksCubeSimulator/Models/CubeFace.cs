b1240e7119f7d68690dfe0cc8b58d87c893c9a07
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-03-20 14:20:56 +0200

    Remove unnecessary usings

--- RubiksCubeSimulator/Models/CubeFace.cs
+++ RubiksCubeSimulator/Models/CubeFace.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RubiksCubeSimulator.Models
+﻿namespace RubiksCubeSimulator.Models
 {
     // Enum for cube faces
     public enum CubeFace
